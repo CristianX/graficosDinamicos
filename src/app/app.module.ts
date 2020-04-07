4027c2e8080f65b24b28137dc7585d617b438c57
CristianX <thecristianx@hotmail.com>
2020-04-07 17:21:29 -0500

    Agregando gráficas de lineas y de barras al proyecto, con sus respectivos componentes e instalando dependencias de ng2charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Gráficos
+import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    BarraComponent,
+    DonaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
