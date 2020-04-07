4fda289c353ff270b8ed9350f78a6a07227a90ff
CristianX <thecristianx@hotmail.com>
2020-04-07 17:36:35 -0500

    Agregando gráfico de dona

--- src/app/components/dona/dona.component.ts
+++ src/app/components/dona/dona.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
 
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html',
   styleUrls: ['./dona.component.css']
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
 
-  ngOnInit(): void {
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public numerosRandom() {
+    this.doughnutChartData = [
+      [Math.round(Math.random() * 100), Math.round(Math.random() * 100), Math.round(Math.random() * 100) ]
+    ];
   }
 
 }
